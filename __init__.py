1e16d675b6250996f7d2dfa51836e9c2bc9ec021
wataru_m <68420282+wtr-m@users.noreply.github.com>
2023-05-11 10:04:12 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 import vector_map
-vector_map.map_convert.gen_sk_map()
-vector_map.area_classification.gen_configration_map()
-vector_map.area_extraction.extract_corner()
+import vector_map.map_convert.gen_sk_map
+import vector_map.area_classification.gen_configration_map
+import vector_map.area_extraction.extract_corner
