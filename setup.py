31fe9fa635db187b290f92496f5d33c755ca1be4
wataru_m <c0a20147d6@edu.teu.ac.jp>
2023-05-24 23:17:04 +0900

    edit setup.py2

--- setup.py
+++ setup.py
@@ -11,5 +11,11 @@ setup(
             'opencv-python',
             'opencv-contrib-python',
             'sympy',
-            ]
+            ],
+        author= ['Wataru MITA',
+                'Johei MATSUOKA'
+                'Kazuya TAGO'],
+        author_email= 'c0a20147d6@edu.teu.ac.jp',
+        url='https://github.com/RobotSpatialCognition/vector_map',
+
 )
