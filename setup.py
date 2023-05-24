e891b8e94f0e66fc505df3e15d5b8811940f65f3
wataru_m <c0a20147d6@edu.teu.ac.jp>
2023-05-24 23:32:29 +0900

    edit setup.py

--- setup.py
+++ setup.py
@@ -17,5 +17,25 @@ setup(
                 'Kazuya TAGO'],
         author_email= 'c0a20147d6@edu.teu.ac.jp',
         url='https://github.com/RobotSpatialCognition/vector_map',
+        keywords=['vectorize',
+                    'SLAM',
+                    'Action Planning',
+                    'ROS' ],
+        classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries'
+        ],
+        description=(
+            "A development tool to create ROS package based on "
+            "behaviour trees framework."
+        ),
+        long_description=(
+            "A development tool to create ROS package based on "
+            "behaviour trees framework."
+        ),
+        license='MIT',
 
 )
