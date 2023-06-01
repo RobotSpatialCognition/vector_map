b1f918e0372915276258c70f2afa4f72846585b9
Kazuya Tago <ktago.gm@gmail.com>
2023-06-01 10:06:47 +0900

    update api.y

--- vector_map/api.py
+++ vector_map/api.py
@@ -6,6 +6,7 @@ import numpy as np
 import os
 import cv2
 import yaml
+import copy
 
 from . import vectorize
 from .geometric_map import World
@@ -20,50 +21,73 @@ class Raster:
 		
 		self.shape = data.shape
 		# self.height = data.shape[1] * resolution
-		self.rotation = 0.0
-		self.cliped_origin_x = 0
-		self.cliped_origin_y = 0
+		self.rotation = None
+		self.clipped_origin_x = 0
+		self.clipped_origin_y = 0
 	#data:boolの配列にすべき
 	
 	def pix_to_coord(self, pix_x, pix_y):
-		reverse_mat = [[np.cos(self.rotation),-1 * np.sin(self.rotation)],[np.sin(self.rotation),np.cos(self.rotation)]]
-		pix_x,pix_y = np.dot(reverse_mat,np.array([pix_y,pix_x])) # ndarray(y, x)
-		pix_x += self.cliped_origin_x
-		pix_y += self.cliped_origin_y
-#		coord_x = float(pix_x)*self.resolution + self.offset_x
-		coord_x = float(pix_x)*self.resolution
+		if self.rotation:
+			#pix_x += self.clipped_origin_x
+			#pix_y += self.clipped_origin_y
+			reverse_mat = [[np.cos(self.rotation),-1 * np.sin(self.rotation)],[np.sin(self.rotation),np.cos(self.rotation)]]
+			pix_x,pix_y = np.dot(reverse_mat,np.array([pix_y,pix_x])) # ndarray(y, x)
+		else:
+			x = pix_x
+			pix_x = pix_y
+			pix_y = x
+		coord_x = float(pix_x)*self.resolution + self.offset_x
 		height = self.data.shape[0] * self.resolution
-#		coord_y = -float(pix_y)*self.resolution + height + self.offset_y
-		coord_y = -float(pix_y)*self.resolution + height
+		coord_y = -float(pix_y)*self.resolution + height + self.offset_y
 		return coord_x, coord_y
 	
 	def move(self, x, y):
-		self.offset_x += x
-		self.offset_y += y
+		if self.rotation:
+			self.post_offset_x += x
+			self.post_offset_y += y
+		else:
+			self.offset_x += x
+			self.offset_y += y
 	
 	def rotate(self, angle):
-		self.rotate = angle
+		if angle == 0: return
+		self.rotation = angle
 		self.data = vectorize.rotation(self.data, self.rotate)
+		self.post_offset_x = 0 # offset for post rotation
+		self.post_offset_y = 0
 
 	def clip(self):
 		self.data, (clipped_origin_x,clipped_origin_y) = vectorize.img_crop(self.data)
-		self.offset_x += clipped_origin_x
-		self.offset_y += clipped_origin_y
+		# adjust offset to cancel clipping effect
+		self.move(clipped_origin_x*self.resolution, clipped_origin_y*self.resolution)
 	
+	def denoize(self, ksize):
+		self.data = vectorize.gen_sk_map(self.data, ksize)
+	
+	def pad(self):
+		self.data = np.pad(self.data, 10, constant_values=0)
+		self.offset_x += 10 * self.resolution 
+		self.offset_y += 10 * self.resolution 
+
 class PixType(Enum):
 	INNER = 2
 	OUTER = 1
 	WALL   = 12
 	CORNER = 15
 
-
 class VectorMap:
 	def __init__(self,raster,ksize=11, epsilon=3):
 		self.raster = raster
 
-		bin_raster = self.create_target_raster()
+		# create target raster to operate: bin_raster
+		bin_raster = copy.copy(raster)
+		bin_raster.clip()
+		bin_raster.denoize(ksize) # ksize=5?
+		self.denoised_raster = copy.copy(bin_raster)
+		bin_raster.pad()
 		self.bin_raster = bin_raster
 
+		# generate corner coordinate list
 		tmp_property, corner_list = vectorize.addition_property(bin_raster.data)
 		_, clist, _ = vectorize.approximate_corner(tmp_property, corner_list)
 		self.corners = np.empty((len(clist),2),dtype=np.int64)
@@ -71,30 +95,13 @@ class VectorMap:
 			self.corners[n][0] = c[0]
 			self.corners[n][1] = c[1]
 
-	def create_target_raster(self,ksize = 5):
-		data = self.raster.data #ndarray
-		resolution = self.raster.resolution
-		center, r  = vectorize.make_mapbb(data)
-
-		croped_raster,offset = vectorize.img_crop(data)
-		denoised_raster = vectorize.gen_sk_map(croped_raster, ksize)
-		self.denoised_raster = denoised_raster
-		bin_raster_data = np.pad(denoised_raster, 10, constant_values=0)
-		origin_x = self.raster.offset_x
-		origin_y = self.raster.offset_y
-		origin_x += 10 * self.raster.resolution 
-		origin_y += 10 * self.raster.resolution 
-		bin_raster = Raster(bin_raster_data, resolution)
-		bin_raster.move(origin_x,origin_y)
-		return bin_raster
-
 	def get_denoised_raster(self):
-		raster = Raster(self.denoised_raster, self.raster.resolution, self.raster.origin)
-		return raster
+		return self.denoised_raster
 
 	def get_corners(self):
 		# デカルト座標でoriginを原点とした座標点のリスト
 		points = []
+		print(len(self.corners))
 		for px,py in self.corners:
 			points.append(self.bin_raster.pix_to_coord(px, py))
 		print(points)
